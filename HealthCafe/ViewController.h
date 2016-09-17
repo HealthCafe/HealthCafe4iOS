1a78ca0e4305be28f46df2070fb17e162ef844a8
oden <oden@jitakukeibi.in>
2016-09-17 12:44:46 +0900

    Initial Commit

---
+++ HealthCafe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HealthCafe
+//
+//  Created by oden on 2016/09/17.
+//  Copyright © 2016年 JITAKUKEIBI, INC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HealthCafe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HealthCafe
+//
+//  Created by oden on 2016/09/17.
+//  Copyright © 2016年 JITAKUKEIBI, INC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
