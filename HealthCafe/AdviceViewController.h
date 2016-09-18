241a7b61121fab92f81a3216a3753b872a3f9b82
oden <oden@jitakukeibi.in>
2016-09-18 13:05:06 +0900

    AdviceViewController追加

---
+++ HealthCafe/AdviceViewController.h
@@ -0,0 +1,13 @@
+//
+//  AdviceViewController.h
+//  HealthCafe
+//
+//  Created by oden on 2016/09/18.
+//  Copyright © 2016年 JITAKUKEIBI, INC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdviceViewController : UIViewController
+
+@end
